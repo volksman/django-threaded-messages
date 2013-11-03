8c58a87b0e2712f1ad5409783096355276e70a13
Shayne <shayne@nomad.ca>
2013-11-03 00:43:51 +0000

    fix for django1.5

--- threaded_messages/models.py
+++ threaded_messages/models.py
@@ -1,18 +1,14 @@
 import datetime
 from django.db import models
 from django.conf import settings
-from django.db.models import signals
-from django.db.models.query import QuerySet
-from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
 from django.db.models import F, Q
-from django.db.models import Avg, Max, Min, Count
 
 from listeners import start_listening
 start_listening()
 
 class MessageManager(models.Manager):
-    
+
     def inbox_for(self, user, read=None, only_unreplied=None):
         """
         Returns all messages that were received by the given user and are not
@@ -36,9 +32,9 @@ class MessageManager(models.Manager):
             if only_unreplied == True:
                 inbox = inbox.filter(Q(replied_at__isnull=True)
                                     |Q(replied_at__lt=F("thread__latest_msg__sent_at")))
-                
+
         return inbox
-    
+
     def outbox_for(self, user):
         """
         Returns all messages that were sent by the given user and are not
@@ -49,7 +45,7 @@ class MessageManager(models.Manager):
             replied_at__isnull=False,
             deleted_at__isnull=True,
         )
-    
+
     def trash_for(self, user):
         """
         Returns all messages that were either received or sent by the given
@@ -66,18 +62,18 @@ class Message(models.Model):
     A private message from user to user
     """
     body = models.TextField(_("body"))
-    sender = models.ForeignKey(User, related_name='sent_messages', blank=True, null=True, verbose_name=_("sender"))
+    sender = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='sent_messages', blank=True, null=True, verbose_name=_("sender"))
     parent_msg = models.ForeignKey('self', related_name='next_messages', blank=True, null=True, verbose_name=_("parent message"))
     sent_at = models.DateTimeField(_("sent at"), auto_now_add=True)
-    
+
     def __unicode__(self):
         return "%s - %s" % (str(self.sender), self.sent_at)
-    
+
     def save(self, **kwargs):
         if not self.id:
             self.sent_at = datetime.datetime.now()
         super(Message, self).save(**kwargs)
-    
+
     class Meta:
         ordering = ['-sent_at']
         verbose_name = _("Message")
@@ -92,34 +88,34 @@ class Thread(models.Model):
     latest_msg = models.ForeignKey(Message, related_name='thread_latest', verbose_name=_("Latest message"))
     all_msgs = models.ManyToManyField(Message, related_name='thread', verbose_name=_("Messages"))
     # the following fields are used to filter out messages that have not been replied to in the inbox
-    creator = models.ForeignKey(User, related_name='created_threads', verbose_name=_("creator"))
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='created_threads', verbose_name=_("creator"))
     replied = models.BooleanField(editable=False, default=False)
-    
+
     def __unicode__(self):
         return self.subject
-    
+
     def get_absolute_url(self):
         return ('messages_detail', [self.id])
     get_absolute_url = models.permalink(get_absolute_url)
-    
+
     class Meta:
         ordering = ['latest_msg']
         verbose_name = _("Thread")
         verbose_name_plural = _("Threads")
-    
-    
+
+
 class Participant(models.Model):
     """
     Thread manager for each participant
     """
     thread = models.ForeignKey(Thread, related_name='participants', verbose_name=_("message thread"))
-    user = models.ForeignKey(User, related_name='threads', verbose_name=_("participant users"))
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='threads', verbose_name=_("participant users"))
     read_at = models.DateTimeField(_("read at"), null=True, blank=True)
     replied_at = models.DateTimeField(_("replied at"), null=True, blank=True)
     deleted_at = models.DateTimeField(_("deleted at"), null=True, blank=True)
-    
+
     objects = MessageManager()
-    
+
     def new(self):
         """returns whether the recipient has read the message or not"""
         if self.read_at is None or self.read_at < self.thread.latest_msg.sent_at:
@@ -132,7 +128,7 @@ class Participant(models.Model):
             or self.replied_at < self.thread.latest_msg.sent_at:
             return True
         return False
-    
+
     def last_other_sender(self):
         """returns the last sender thats not the viewing user. if nobody
             besides you sent a message to the thread we take a random one
@@ -145,7 +141,7 @@ class Participant(models.Model):
             if others:
                 return others[0].user
         return None
-    
+
     def others(self):
         """returns the other participants of the thread"""
         return self.thread.participants.exclude(user=self.user)
@@ -166,10 +162,10 @@ class Participant(models.Model):
             return participation
         except:
             return None
-        
+
     def __unicode__(self):
         return "%s - %s" % (str(self.user), self.thread.subject)
-    
+
     class Meta:
         ordering = ['thread']
         verbose_name = _("participant")
